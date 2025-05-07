98037f4eb97a92490e511c5142268a41e9a73a54
CharanLingolu <151988042+CharanLingolu@users.noreply.github.com>
2025-05-07 20:57:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Update the base URL to match your GitHub repository name
 export default defineConfig({
+  base: '/Shopping_App_React/', // Change this to your repo name
   plugins: [react()],
-})
+});
