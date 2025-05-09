cccfc0657207afa20be5a381978f06920a9b39de
charan lingolu <charanlingolu@gmail.com>
2025-05-09 15:59:29 +0530

    Updated project files

--- src/shoppingfolder/components/Header.jsx
+++ src/shoppingfolder/components/Header.jsx
@@ -1,38 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div>
       <div className='headerSection'>
         <div className='left'>
           <div className='title'>
-            <h2>Shopping Mall</h2>
+            <h2>Shopping App</h2>
           </div>
         </div>
         <div className='center'>
-          <ul>
+          <ul className={menuOpen ? 'hidden' : ''}>
             <li>Women</li>
+            <li>Men</li>
+            <li>Children</li>
+            <li>Beauty</li>
+          </ul>
+          <div className='toggleBtn' onClick={() => setMenuOpen(!menuOpen)}>
+            <i className={`fa-solid ${menuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+          </div>
+        </div>
+        <div className='search'>
+          <input type='text' placeholder='search...' />
+        </div>
+        <div className='right'>
+          <div className='signin'>Signin / Signup</div>
+          <div className='cart'>Cart</div>
+        </div>
+      </div>
+      <div className={`dropdownmenu ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li>Women</li>
           <li>Men</li>
           <li>Children</li>
           <li>Beauty</li>
-          </ul>
-        </div>
-        <div className="search">
-          <input type='text' placeholder='search...'/>
-        </div>
-        <div className='right'>
-          <div className='signin'>
-            <div>
-              Signin / Signup
-            </div>
-              </div>
-            <div className='cart'>
-              Cart
-          </div>
-        </div>
+        </ul>
+        <div className='signindrop'>Signin / Signup</div>
+        <div className='cartdrop'>Cart</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
